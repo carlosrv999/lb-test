13bb993a8435480d49f5fe9f9ad84759528f4264
Carlos Ramirez <carlos.ramirez1@unmsm.edu.pe>
2019-04-10 21:28:07 +0000

    test oki

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,8 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/oki', (req, res) => {
+    return res.status(200).send({status: 'ok'});
+  })
   server.use(router);
 };
