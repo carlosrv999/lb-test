0b1a3257c918b537a4842b3b8adae0ac00735154
Carlos Ramirez <carlos.ramirez1@unmsm.edu.pe>
2019-04-11 17:42:01 +0000

    deleted oki

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,8 +4,5 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.get('/oki', (req, res) => {
-    return res.status(200).send({status: 'ok'});
-  })
   server.use(router);
 };
