9bbe9356113315feabfe04efdea652c08acd711f
Carlos Ramirez <carlos.ramirez1@unmsm.edu.pe>
2019-04-19 20:43:56 +0000

    deleted opencloud

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,8 +3,8 @@
 module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.get('/opencloud', (req,res) => {
-    return res.status(200).send({"message": "Hello Open Cloud"});
-  })
+  //router.get('/opencloud', (req,res) => {
+  //  return res.status(200).send({"message": "Hello Open Cloud"});
+  //})
   server.use(router);
 };
